eef04006f5cea74f9f902266e49a052d8afeb898
AppDev <portalbisnes@gmail.com>
2020-11-06 20:00:00 -0600

    update

---
+++ danog-php/madelineproto/src/danog/MadelineProto/Db/DbArray.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace danog\MadelineProto\Db;
+
+use Amp\Iterator;
+use Amp\Promise;
+
+/**
+ * DB array interface.
+ *
+ * @template T as mixed
+ */
+interface DbArray extends DbType, \ArrayAccess, \Countable
+{
+    /**
+     * Get Array copy.
+     *
+     * @psalm-return Promise<array<string|int, T>>
+     *
+     * @return Promise
+     */
+    public function getArrayCopy(): Promise;
+    /**
+     * Check if element is set.
+     *
+     * @param string|int $key
+     *
+     * @psalm-return Promise<bool>
+     *
+     * @return Promise
+     */
+    public function isset($key): Promise;
+    /**
+     * Get element.
+     *
+     * @param string|int $index
+     *
+     * @psalm-return Promise<T>
+     *
+     * @return Promise
+     */
+    public function offsetGet($index): Promise;
+    /**
+     * Set element.
+     *
+     * @param string|int $index
+     * @param mixed      $value
+     *
+     * @psalm-param T $value
+     *
+     * @return Promise
+     */
+    public function offsetSet($index, $value);
+    /**
+     * Unset element.
+     *
+     * @param string|int $index Offset
+     * @return Promise
+     */
+    public function offsetUnset($index): Promise;
+    /**
+     * Count number of elements.
+     *
+     * @return Promise<integer>
+     */
+    public function count(): Promise;
+    /**
+     * Clear all elements.
+     *
+     * @return Promise
+     */
+    public function clear(): Promise;
+    /**
+     * Get iterator.
+     *
+     * @return Iterator<array{0: string|int, 1: T}>
+     */
+    public function getIterator(): Iterator;
+
+    /**
+     * @deprecated
+     * @internal
+     * @see DbArray::isset();
+     *
+     * @param mixed $index
+     *
+     * @return bool
+     */
+    public function offsetExists($index);
+}
---
+++ danog-php/madelineproto/src/danog/MadelineProto/Db/MemoryArray.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace danog\MadelineProto\Db;
+
+use Amp\Iterator;
+use Amp\Producer;
+use Amp\Promise;
+use Amp\Success;
+use danog\MadelineProto\Logger;
+use danog\MadelineProto\Settings\Database\Memory;
+
+use function Amp\call;
+
+/**
+ * Memory database backend.
+ */
+class MemoryArray extends \ArrayIterator implements DbArray
+{
+    public function __construct($array = [], $flags = 0)
+    {
+        parent::__construct((array) $array, $flags | self::STD_PROP_LIST);
+    }
+
+    /**
+     * Get instance.
+     *
+     * @param string $table
+     * @param mixed  $previous
+     * @param Memory $settings
+     * @return Promise<self>
+     */
+    public static function getInstance(string $table, $previous, $settings): Promise
+    {
+        return call(static function () use ($previous) {
+            if ($previous instanceof MemoryArray) {
+                return $previous;
+            }
+            if ($previous instanceof DbArray) {
+                Logger::log("Loading database to memory. Please wait.", Logger::WARNING);
+                if ($previous instanceof DriverArray) {
+                    yield from $previous->initStartup();
+                }
+                $temp = yield $previous->getArrayCopy();
+                yield $previous->clear();
+                $previous = $temp;
+            }
+            return new static($previous);
+        });
+    }
+
+    public function offsetExists($offset)
+    {
+        throw new \RuntimeException('Native isset not support promises. Use isset method');
+    }
+
+    public function isset($key): Promise
+    {
+        return new Success(parent::offsetExists($key));
+    }
+
+    public function offsetGet($offset): Promise
+    {
+        return new Success(parent::offsetExists($offset) ? parent::offsetGet($offset) : null);
+    }
+
+    public function offsetUnset($offset): Promise
+    {
+        return new Success(parent::offsetUnset($offset));
+    }
+
+    public function count(): Promise
+    {
+        return new Success(parent::count());
+    }
+
+    public function getArrayCopy(): Promise
+    {
+        return new Success(parent::getArrayCopy());
+    }
+
+    public function clear(): Promise
+    {
+        parent::__construct([], parent::getFlags());
+        return new Success();
+    }
+
+    public function getIterator(): Iterator
+    {
+        return new Producer(function (callable $emit) {
+            foreach ($this as $key => $value) {
+                yield $emit([$key, $value]);
+            }
+        });
+    }
+}
